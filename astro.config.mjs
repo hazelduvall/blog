8dbcc281967eddbe5444f1f35ea1d74bca6d0469
Hazel Duvall <10897431+hazelduvall@users.noreply.github.com>
2026-05-02 21:59:54 -0400

    fix sitemap urls

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,23 @@ import { defineConfig } from "astro/config";
 import rehypeKatex from "rehype-katex";
 import remarkMath from "remark-math";
 
+const site = "https://www.hazelduvall.dev";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.hazelduvall.dev",
-  integrations: [mdx(), sitemap()],
+  site,
+  integrations: [
+    mdx(),
+    sitemap({
+      serialize(item) {
+        // there is a bug where it doesn't get the file page names correct. whoops!
+        if (item.url !== site + "/") {
+          item.url = item.url + ".html";
+        }
+        return item;
+      },
+    }),
+  ],
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
